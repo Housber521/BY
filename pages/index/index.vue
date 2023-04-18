6ca1e3d75a3148ccded3d5091e4f5299e64a481f
shiguang <2505551078@qq.com>
2023-04-18 20:02:26 +0800

    第三次上传

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			<text class="title">我们今天来写必要商城</text>
 		</view>
 	</view>
 </template>
